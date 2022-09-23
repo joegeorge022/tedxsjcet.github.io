2f77ec1230e836de29e4aa13440adbb6c83f08cd
reenphygeorge <reenphygeorge@gmail.com>
2022-09-23 10:45:29 +0530

    feat: new navbar added

---
+++ src/scripts/nav.js
@@ -0,0 +1,8 @@
+function rotate() {
+    // document.getElementById("l1").classList.toggle("rotate1");
+    // document.getElementById("l2").classList.toggle("rotate2");
+    // document
+    //     .getElementById("mob-nav")
+    //     .classList.toggle("mobile-nav-active");
+    document.getElementById("mob-nav-ul").classList.toggle("d-none")
+}
