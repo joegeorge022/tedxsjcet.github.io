3b54db2d4bec057315a92991a1d15f9c39039f0c
reenphygeorge <reenphygeorge@gmail.com>
2022-09-27 23:51:47 +0530

    fix: Mobile Navbar

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,9 +1,9 @@
 function rotate() {
     // document.getElementById("l1").classList.toggle("rotate1");
     // document.getElementById("l2").classList.toggle("rotate2");
-    // document
-    //     .getElementById("mob-nav")
-    //     .classList.toggle("mobile-nav-active");
+    document
+        .getElementById("mob-nav")
+        .classList.toggle("mobile-nav-active");
     document.getElementById("mob-nav-ul").classList.toggle("nav-ul-mob-toggle")
     document.getElementById("main-sect-h1").classList.toggle("padding-nav")
 }
