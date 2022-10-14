2c0fc0ad5f741267c8850e36cdcb67582ee4d987
reenphygeorge <reenphygeorge@gmail.com>
2022-10-14 18:13:57 +0530

    fix: navbar spacing

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -2,5 +2,5 @@ function display() {
     const myTimeout = setTimeout(() => {
         document.querySelector('.preloader').classList.add('d-none');
         document.querySelector('.main').classList.remove('d-none');
-    }, 2000);
+    }, 5000);
 }
