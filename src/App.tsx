58187836f53c1ff9ac935bc7abd0e736f47713e5
Milansuman <milansuman2016@gmail.com>
2024-12-08 11:26:56 +0530

    initial solidjs and tailwind css setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class='flex w-full h-screen bg-black'>
+
+    </div>
+  );
+};
+
+export default App;
