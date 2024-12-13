ef449d3c583d09f0760e299b744747fb3761b315
Milansuman <milansuman2016@gmail.com>
2024-12-13 19:02:08 +0530

    attempt to bring everything to root directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import type { Component } from 'solid-js';
 const App: Component = () => {
     return (
         <div class='flex w-full h-screen bg-black overflow-auto text-white'>
-            {/* Initial Section */}
+            {/* Hero Section */}
             <div class='flex flex-col w-full h-screen md:flex-row-reverse'>
                 <div class='h-1/2 md:h-full md:w-1/2 flex bg-slate-300'>
 
@@ -14,12 +14,12 @@ const App: Component = () => {
 
                         </div>
                         <div class='flex w-1/2 bg-slate-700 justify-center items-center'>
-                            <h1 class='-rotate-90 w-fit h-fit text-3xl md:text-5xl font-bold'>Excellence<br/>beyond notice</h1>
+                            <h1 class='-rotate-90 w-fit h-fit text-3xl md:text-4xl font-bold'>Excellence<br/>beyond notice</h1>
                         </div>
                     </div>
                     <div class='h-1/3 flex flex-row'>
                         <div class='flex w-1/2 bg-white justify-center items-center'>
-                            <h2 class='text-black text-2xl md:text-4xl w-fit h-fit font-bold'>
+                            <h2 class='text-black text-2xl md:text-3xl w-fit h-fit font-bold'>
                                 Different<br/> Perspectives
                             </h2>
                         </div>
